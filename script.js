c2d2a05ad7124952d25bc0273472fb0643715f8c
Nitesh Chaudhary <59913877+Nitesh-bit@users.noreply.github.com>
2024-02-06 03:31:58 -0800

    Created a weather app

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey = "b06003190241205a667e4b7b16af037a";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(cityName) {
+  const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
+
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+
+    weatherIcon.src = `images/${data.weather[0].main}.png`;
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
